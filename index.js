f5e43650eb6150d918173fca86e153a1d041319d
dani <dani@duakelinci.co.id>
2025-07-18 13:41:34 +0700

    config root

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ export { insertData, updateData, deleteData, generateCode, generateUrutan } from
 export {  stringToEncrypt, encryptToString, stringToBase64, base64ToString, generateCodeRandom, getBaseUrl, replaceTextFromTemplate } from './src/utils/globalFunction.js';
 export {  successResponse, errorResponse } from './src/utils/responseHandler.js';
 export {  utcToZone, zoneToUtc } from './src/utils/timezoneFormat.js';
+export { default as connection } from './src/config/connection.js';
+export { default as header } from './src/middlewares/header.js';
+export { default as validate } from './src/middlewares/validate.js';
