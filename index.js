065dbaf687b1e571b13958adb5b6946499462f27
dani <dani@duakelinci.co.id>
2025-07-18 11:12:41 +0700

    re config

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 export { insertData, updateData, deleteData, generateCode, generateUrutan } from './src/utils/dynamicModel.js';
+export {  stringToEncrypt, encryptToString, stringToBase64, base64ToString, generateCodeRandom, getBaseUrl, replaceTextFromTemplate } from './src/utils/globalFunction.js';
+export {  successResponse, errorResponse } from './src/utils/responseHandler.js';
+export {  utcToZone, zoneToUtc } from './src/utils/timezoneFormat.js';
