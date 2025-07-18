a2a7e2e4e296058df33ecc65a4566259baa0c13c
dani <dani@duakelinci.co.id>
2025-07-18 13:41:07 +0700

    adding config

---
+++ src/config/connection.js
@@ -0,0 +1,17 @@
+
+export default async function connection(db) {
+    try {
+        const conn = await db.getConnection();
+        return conn;
+    } catch (err) {
+        if (err.code === 'ECONNREFUSED') {
+            throw { status: 503, message: 'Database service unavailable. Please try again later.' };
+        }
+
+        if (err.code === 'ER_ACCESS_DENIED_ERROR') {
+            throw { status: 401, message: 'Invalid database credentials.' };
+        }
+
+        throw { status: 401, message: 'Internal server error when connecting to database.' };
+    }
+};
